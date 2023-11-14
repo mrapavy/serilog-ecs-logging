39685db951415d945dd3087e346f950dc6db0c98
Martin Rapavy <mrapavy@cpicardgroup.com>
2023-11-14 14:23:25 -0600

    Tags initialized with List<string>

--- src/SerilogEcsLogging/Logging/AlertBase.cs
+++ src/SerilogEcsLogging/Logging/AlertBase.cs
@@ -3,7 +3,7 @@
 public class AlertBase : EcsEvent
 {
     public AlertBase(string eventAction, bool eventOutcome = false, TimeSpan? eventDuration = null, string? message = null, object? eventData = null, string? transactionId = null, string? traceId = null) 
-        : base(eventAction, Elastic.CommonSchema.EventKind.Alert, new []{"Error"}, eventOutcome, eventDuration, message, eventData, transactionId, traceId)
+        : base(eventAction, Elastic.CommonSchema.EventKind.Alert, new List<string> {"Error"}, eventOutcome, eventDuration, message, eventData, transactionId, traceId)
     {
     }
 }
--- src/SerilogEcsLogging/Logging/MetricBase.cs
+++ src/SerilogEcsLogging/Logging/MetricBase.cs
@@ -3,7 +3,7 @@
 public class MetricBase : EcsEvent
 {
     public MetricBase(string eventAction, bool eventOutcome = true, TimeSpan? eventDuration = null, string? message = null, object? eventData = null, string? transactionId = null, string? traceId = null) 
-        : base(eventAction, Elastic.CommonSchema.EventKind.Metric, new []{"Metric"}, eventOutcome, eventDuration, message, eventData, transactionId, traceId)
+        : base(eventAction, Elastic.CommonSchema.EventKind.Metric, new List<string> {"Metric"}, eventOutcome, eventDuration, message, eventData, transactionId, traceId)
     {
     }
 }
